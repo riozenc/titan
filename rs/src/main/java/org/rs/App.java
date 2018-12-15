215ab678b0a0244af8cff2c8279d25e1bce7d61c
riozenc <riozenc@riozenc>
2018-12-15 16:38:54 +0800

    4

--- rs/src/main/java/org/rs/App.java
+++ rs/src/main/java/org/rs/App.java
@@ -12,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class App {
+
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 		System.out.println("Hello World!");
