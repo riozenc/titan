7f322992d0b26303cb1dd993bf25e66d2e7d7cbf
riozenc <riozenc@riozenc>
2018-12-15 16:35:11 +0800

    gate

---
+++ gateway/src/main/java/org/gateway/GateWayApp.java
@@ -0,0 +1,30 @@
+package org.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@RestController
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayApp {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(GateWayApp.class);
+
+	public static void main(String[] args) {
+//		RoutePredicateHandlerMapping
+		SpringApplication.run(GateWayApp.class, args);
+		LOGGER.info(" Start GateWayApp Done");
+	}
+
+}
