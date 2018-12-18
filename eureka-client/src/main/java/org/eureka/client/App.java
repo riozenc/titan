50e936ba47184fd2e086959655474d30e10341ca
riozenc <riozenc@riozenc>
2018-12-18 11:18:00 +0800

    C

--- eureka-client/src/main/java/org/eureka/client/App.java
+++ eureka-client/src/main/java/org/eureka/client/App.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-<<<<<<< HEAD
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-=======
 import org.springframework.cloud.openfeign.EnableFeignClients;
->>>>>>> branch 'master' of https://github.com/riozenc/titan.git
+
 
 /**
  * Hello world!
