9d7a8d2d8e7725cf27446f0cb2db826628e92bc7
czy <czy@DESKTOP-9LNOG4H>
2019-06-29 16:59:50 +0800

    不知道改了什么

---
+++ gateway/src/main/java/org/gateway/entity/LoginEntity.java
@@ -0,0 +1,28 @@
+/**
+ * Author : czy
+ * Date : 2019年6月11日 下午2:15:45
+ * Title : org.gateway.entity.LoginEntity.java
+ *
+**/
+package org.gateway.entity;
+
+public class LoginEntity {
+	private String username;
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
