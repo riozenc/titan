cb63b5a6682741e46b7559f5c3cc024a54322beb
czy <czy@DESKTOP-9LNOG4H>
2019-12-03 14:15:19 +0800

    123

--- gateway/src/main/java/org/gateway/filter/ApiFilter.java
+++ gateway/src/main/java/org/gateway/filter/ApiFilter.java
@@ -8,10 +8,12 @@ package org.gateway.filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
 import reactor.core.publisher.Mono;
 
+@Component
 public class ApiFilter implements GatewayFilter{
 
 	@Override
