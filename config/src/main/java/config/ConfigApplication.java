aaa27617a7c9f304ee67f57e6104dc47212ed859
czy <czy@DESKTOP-9LNOG4H>
2020-04-07 15:10:58 +0800

    config center

--- config/src/main/java/config/ConfigApplication.java
+++ config/src/main/java/config/ConfigApplication.java
@@ -7,10 +7,12 @@ package config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "config")
+@EnableEurekaClient
+@EnableConfigServer
 public class ConfigApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigApplication.class, args);
