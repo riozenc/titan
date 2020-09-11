386ab91e1b84e491629ec4743eab27e730ee921e
czy <czy@DESKTOP-9LNOG4H>
2020-09-11 10:39:04 +0800

    剔除mongo

--- config/src/main/java/config/ConfigApplication.java
+++ config/src/main/java/config/ConfigApplication.java
@@ -7,10 +7,11 @@ package config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
-@SpringBootApplication(scanBasePackages = "config")
+@SpringBootApplication(scanBasePackages = "config",exclude = MongoAutoConfiguration.class)
 public class ConfigApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigApplication.class, args);
